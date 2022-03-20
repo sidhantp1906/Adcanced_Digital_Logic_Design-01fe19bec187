76a3cddca48149d0de4dbdd2cff628bf34b3cf03
Sidhant priyadarshi <60102705+sidhantp1906@users.noreply.github.com>
2022-03-20 15:51:03 +0530

    Add files via upload

---
+++ adder_multiplier/Reg1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:16:10 02/05/2022 
+// Design Name: 
+// Module Name:    Reg1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Reg1(input load,[15:0]data,
+input clk,output reg [15:0]X
+    );
+always @(posedge clk)
+begin
+ if(load)
+ X <= data;
+end
+endmodule
